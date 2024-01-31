b6344f514bc2209879e73b9ac44d597cdefb372c
Glessme <roger02michel@outlook.fr>
2024-01-31 14:21:41 +0100

    modification de la première route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/', [MainController::class, 'home'])->name('main.home');
+
+Route::get('/menu', [MainController::class, 'menu'])->name('main.menu');
+
+Route::get('/plat/{id}', [MainController::class, 'plat'])->name('main.plat');
+
+Route::get('/reservation', [MainController::class, 'reservation'])->name('main.reservation');
+
+Route::post('/reservation', [MainController::class, 'reservationStore'])->name('main.reservation.store');
+
+//une route c'est à la fois une url et une fonction.
+//si c'est la même url mais une fonction différente (get, post...) ça fera appel à la bonne fonction
+//en gros la requete fait appel à la class dans le maincontroller. le maincontroller renvoie au navigateur ce qui a été généré par le serveur
