e25470f4f96146987347776110fb3d6a26cf0e71
Glessme <roger02michel@outlook.fr>
2024-01-31 15:42:58 +0100

    création de menu.blade

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+
+@section('content')
+
+@section('title', 'Menu')
+    <section>
+        <h2>Menu</h2>
+        @foreach ($categories as $categorie)
+            <div>
+                <h3>{{ $categorie->nom }}</h3>
+                <ul class="plats">
+                    @foreach ($categorie->plats() as $plat)
+                    <li>
+                        <a href="{{route('main.plat', ['id'=> $plat->id])}}">
+                            <img src="{{ asset('Image/' . $plat->fichier) }}" alt="{{ $plat->nom }}" /></a>
+                            <a href="{{route('main.plat', ['id'=> $plat->id])}}">
+                            <h4>{{ $plat->nom }}</h4></a>
+                            <span class="prix">{{ $plat->prix }}€</span> 
+                            <ul>
+                            @foreach ($plat->etiquettes() as $etiquette)
+                        <li>{{$etiquette->nom}}</li>
+                        @endforeach
+                    </ul>
+                    </li>
+                        @endforeach
+                </ul>
+            </div>
+        @endforeach
+
+    </section>
+
+@endsection
