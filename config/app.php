95e697271345423955f371fb96b43c4dc6a87f71
Glessme <roger02michel@outlook.fr>
2024-02-01 19:10:25 +0100

    ajout des fichiers manquants

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'project-Ocnamo'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+@extends ('base')
+
+@section('content')
+
+
+
+        <h1>
+            <a href="Accueil.html">O Cnamo</a></h1>
+    </center>
+
+  </header>
+
+    <h2>INFOS PRATIQUES</h2>
+    <ol>
+      <li>5 Bd Louis XIV, 59800 Lille</li>
+      <li>03.11.11.11.11 / 03.12.12.12.12</li>
+      <li>Lundi au Dimanche 11h30-15h00 et 19h00-22h30</li>
+    </ol>
+         </article>
+  <form>
+    <div>
+        <button>Reservation</button>
+    </div>
+  </form> 
+  <div>
+    <footer>
+      <p>PARLEZ DE NOUS!</p>
+    </footer>
+  </div>
+  @endsection
